dd4d588dc98d5c501cab1568b9876d393aee2275
TIAKOUANG <djoukevin1469@gmail.com>
2026-07-28 13:24:42 +0100

    refactor(admin): use decorator to register WhatsAppTemplate admin

--- zola-ashe-backend/apps/notifications/admin.py
+++ zola-ashe-backend/apps/notifications/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-
 from .models import WhatsAppTemplate
 
 
-admin.site.register(WhatsAppTemplate)
+@admin.register(WhatsAppTemplate)
+class WhatsAppTemplateAdmin(admin.ModelAdmin):
+    pass
